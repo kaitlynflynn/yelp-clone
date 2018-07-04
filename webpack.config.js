31ce4524a4f06374fc94ab2944064b9301d93ca1
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-07-04 15:31:54 -0700

    Modified webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,17 @@
+const NODE_ENV = process.env.NODE_ENV;
+const isDev = NODE_ENV === 'development';
+// alternatively, we can use process.argv[1]
+// const isDev = (process.argv[1] || '')
+//                .indexOf('hjs-dev-server') !== -1;
+
+// ...
+var config = getConfig({
+  isDev: isDev,
+  in: join(src, 'app.js'),
+  out: dest,
+  clearBeforeBuild: true
+})
+
 const webpack = require('webpack');
 const fs      = require('fs');
 const path    = require('path'),
@@ -5,3 +19,21 @@ const path    = require('path'),
       resolve = path.resolve;
 
 const getConfig = require('hjs-webpack');
+
+const root    = resolve(__dirname);
+const src     = join(root, 'src');
+const modules = join(root, 'node_modules');
+const dest    = join(root, 'dist');
+
+var config = getConfig({
+  in: join(src, 'app.js'),
+  out: dest,
+  clearBeforeBuild: true
+})
+
+var config = getConfig({
+    in: join(__dirname, 'src/app.js'),
+    out: join(__dirname, 'dist')
+  })
+
+module.exports = config;
